43ca73840721597c9a67487d2f1b966214edbb03
kristi1205 <it14103@lehre.dhbw-stuttgart.de>
2016-03-15 10:26:08 +0100

    uebersicht + statistik

---
+++ statistik.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="de_DE">
+<head>
+  <title>My Fitness Diary - Home</title>
+  	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand">My Fitness Diary</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="uebersicht.php">Übersicht</a></li>
+      <li><a href="statistik.php">Statistik</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span> Log out</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="page-header" style="text-align: center;">
+  		<h1>Statistik</h1>
+	</div>
+		<div class="container">
+			<div class="fromwrap center">
+				<form class="form-horizontal" role="form">
+
+
+
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
